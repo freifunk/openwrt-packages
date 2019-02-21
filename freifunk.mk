20c6c1ef51f6f633d91432170dad485db0724a96
Sven Roederer <devel-sven@geroedel.de>
2019-02-22 00:32:57 +0100

    freifunk.mk: remove LuCI-i18n stuff
    this belongs to the LuCI-feed and seems not needed here
    
    Signed-off-by: Sven Roederer <freifunk@it-solutions.geroedel.de>

--- freifunk.mk
+++ freifunk.mk
@@ -106,30 +106,6 @@ ifneq ($(LUCI_DESCRIPTION),)
  endef
 endif
 
-# Language selection for luci-base
-ifeq ($(PKG_NAME),luci-base)
- define Package/luci-base/config
-   config LUCI_SRCDIET
-	bool "Minify Lua sources"
-	default n
-
-   config LUCI_JSMIN
-	bool "Minify JavaScript sources"
-	default y
-
-   config LUCI_CSSTIDY
-        bool "Minify CSS files"
-        default y
-
-   menu "Translations"$(foreach lang,$(LUCI_LANGUAGES),
-
-     config LUCI_LANG_$(lang)
-	   tristate "$(shell echo '$(LUCI_LANG.$(lang))' | sed -e 's/^.* (\(.*\))$$/\1/') ($(lang))")
-
-   endmenu
- endef
-endif
-
 define Build/Prepare
 	for d in luasrc htdocs root src; do \
 	  if [ -d ./$$$$d ]; then \
@@ -224,41 +200,4 @@ endif
 
 LUCI_BUILD_PACKAGES := $(PKG_NAME)
 
-define LuciTranslation
-  define Package/luci-i18n-$(LUCI_BASENAME)-$(1)
-    SECTION:=luci
-    CATEGORY:=LuCI
-    TITLE:=$(PKG_NAME) - $(1) translation
-    HIDDEN:=1
-    DEFAULT:=LUCI_LANG_$(1)||(ALL&&m)
-    DEPENDS:=$(PKG_NAME)
-    PKGARCH:=all
-  endef
-
-  define Package/luci-i18n-$(LUCI_BASENAME)-$(1)/description
-    Translation for $(PKG_NAME) - $(LUCI_LANG.$(1))
-  endef
-
-  define Package/luci-i18n-$(LUCI_BASENAME)-$(1)/install
-	$$(INSTALL_DIR) $$(1)/etc/uci-defaults
-	echo "uci set luci.languages.$(subst -,_,$(1))='$(LUCI_LANG.$(1))'; uci commit luci" \
-		> $$(1)/etc/uci-defaults/luci-i18n-$(LUCI_BASENAME)-$(1)
-	$$(INSTALL_DIR) $$(1)$(LUCI_LIBRARYDIR)/i18n
-	$(foreach po,$(wildcard ${CURDIR}/po/$(1)/*.po), \
-		po2lmo $(po) \
-			$$(1)$(LUCI_LIBRARYDIR)/i18n/$(basename $(notdir $(po))).$(1).lmo;)
-  endef
-
-  define Package/luci-i18n-$(LUCI_BASENAME)-$(1)/postinst
-	[ -n "$$$${IPKG_INSTROOT}" ] || {
-		(. /etc/uci-defaults/luci-i18n-$(LUCI_BASENAME)-$(1)) && rm -f /etc/uci-defaults/luci-i18n-$(LUCI_BASENAME)-$(1)
-		exit 0
-	}
-  endef
-
-  LUCI_BUILD_PACKAGES += luci-i18n-$(LUCI_BASENAME)-$(1)
-
-endef
-
-$(foreach lang,$(LUCI_LANGUAGES),$(eval $(call LuciTranslation,$(lang))))
 $(foreach pkg,$(LUCI_BUILD_PACKAGES),$(eval $(call BuildPackage,$(pkg))))
